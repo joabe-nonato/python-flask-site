5efdb92480974d73c4cdf247853693fc04563a54
joabe <joabe07@gmail.com>
2022-10-02 18:46:41 -0300

    Adicionando tela de login

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
 from classes import Jogo
 
 app = Flask(__name__)
 
-tlt = 'Site em Flask'
+# Adicionado criptografia
+app.secret_key = "estudo"
+
+tlt = 'Jogoteca'
 jogos = []
 
 jogos.append(Jogo('Donkey Kong','Plataforma','SNES'))
@@ -26,5 +29,21 @@ def create():
     jogos.append(jg)
     # return render_template('index.html', titulo = tlt, lista = jogos )
     return redirect('/')
+
+@app.route('/login')
+def login():
+    return render_template('login.html', titulo = tlt)
+
+@app.route('/autenticar', methods=['POST'])
+def autenticar():
+    session['usuario_logado'] = ''
+
+    if request.form['senha'] == '123':
+        session['usuario_logado'] = request.form['usuario']
+        flash(session['usuario_logado'] + ' logado com sucesso.')
+        return redirect('/')
+    else:
+        flash('Login ou senha inválido')
+        return redirect('/login')
 
 app.run(debug=True)
