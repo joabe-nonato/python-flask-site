6ee074512cf3580371ccf93afea3397098be3125
joabe <joabe07@gmail.com>
2022-10-09 14:38:06 -0300

    adicionando templates

--- main.py
+++ main.py
@@ -5,38 +5,57 @@ app = Flask(__name__)
 
 # Adicionado criptografia
 app.secret_key = "estudo"
-
 tlt = 'Jogoteca'
-jogos = []
 
+jogos = []
 jogos.append(Jogo('Donkey Kong','Plataforma','SNES'))
 jogos.append(Jogo('Sonic', 'Plataforma', 'Genesis'))
 
+
 @app.route('/')
 def index():
+    VerificarUsuario()
     return render_template('index.html', titulo = tlt, lista = jogos )
 
-@app.route('/cadastro')
-def cadastro():
-    return render_template('cadastro.html', titulo = tlt)
 
-@app.route('/create', methods=['POST'])
-def create():
+@app.route('/jogos')
+def listajogos():
+    if 'usuario_logado' not in session or session['usuario_logado'] is None:
+        return redirect('/login')
+    return render_template('/jogos/index.html', titulo = tlt, lista = jogos )
+
+
+@app.route('/jogos/cadastro')
+def cadastro():
+    if 'usuario_logado' not in session or session['usuario_logado'] is None:
+        return redirect('/login')
+    return render_template('/jogos/cadastro.html', titulo = tlt)
+
+
+@app.route('/cadastrar-jogo', methods=['POST'])
+def cadastrarjogo():
     nome = request.form['nome']
     categoria = request.form['categoria']
     console = request.form['console']
     jg = Jogo(nome, categoria, console)
     jogos.append(jg)
-    # return render_template('index.html', titulo = tlt, lista = jogos )
-    return redirect('/')
+    return redirect('/jogos')
+
+
+@app.route('/deslogar', methods=['POST'])
+def deslogar():
+    session['usuario_logado'] = None
+    return redirect('/login')
+
 
 @app.route('/login')
 def login():
     return render_template('login.html', titulo = tlt)
 
+
 @app.route('/autenticar', methods=['POST'])
 def autenticar():
-    session['usuario_logado'] = ''
+    session['usuario_logado'] = None
 
     if request.form['senha'] == '123':
         session['usuario_logado'] = request.form['usuario']
@@ -46,4 +65,13 @@ def autenticar():
         flash('Login ou senha inválido')
         return redirect('/login')
 
+
+def VerificarUsuario():
+    if 'usuario_logado' not in session or session['usuario_logado'] is None:
+        return redirect('/login')
+
+
 app.run(debug=True)
+
+
+
